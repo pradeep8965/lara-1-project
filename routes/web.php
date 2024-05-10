a933531f8f7b7943f649f95d255f56fce3930e94
“misterpp143@gmail.comgit config --global user.email misterpp143@gmail.comgit config  credential.helper ‘store’git config --global user.name “misterpp143@gmail.com <misterpp143@gmail.com>
2024-05-10 16:13:12 +0530

    day 6

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,23 @@ Route::get('/myfriend', function () {
 }); 
 Route::get('/student',[StudentController::class,'student1']
 );
+Route::prefix('/mp')->group(function(){
+    Route::get('/neemuch', function (){
+        return view ('mp.neemuch');
+    });
+    Route::get('/indore', function (){
+        return 'Hello from indore ';
+    });
+    Route::get('/ratlam', function (){
+        return 'Hello from ratlam ';
+    });
+    Route::get('/bhpal', function (){
+        return 'Hello from bhopal ';
+    });
+    Route::get('/kanjarda', function (){
+        return 'Hello from kanjarda ';
+    });
+    Route::get('/manasa', function (){
+        return 'Hello from manasa ';
+    });
+});
