859344723f38b997a5cd5e815be1d3f99a3ab94c
“misterpp143@gmail.comgit config --global user.email misterpp143@gmail.comgit config  credential.helper ‘store’git config --global user.name “misterpp143@gmail.com <misterpp143@gmail.com>
2024-05-10 14:23:23 +0530

    new lara 1 project

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/pradeep', function () {
+    return view('pradeep');
+});
+Route::get('/myfriend', function () {
+    return view('myfriend');
+}); 
+Route::get('/myfriend2', function () {
+    return'hello good morning 2';
+}); 
+Route::get('/myfriend3', function () {
+    return'hello good morning 3';
+}); 
+Route::get('/myfriend4', function () {
+    return'hello good morning 4';
+}); 
+Route::get('/myfriend5', function () {
+    return'hello good morning 5';
+}); 
+Route::get('/myfriend6', function () {
+    return'hello good morning 6';
+}); 
+Route::get('/myfriend7', function () {
+    return'hello good morning 7';
+}); 
+Route::get('/myfriend8', function () {
+    return'hello good morning 8';
+}); 
+Route::get('/myfriend9', function () {
+    return'hello good morning 9';
+}); 
+Route::get('/myfriend10', function () {
+    return'hello good morning 10';
+}); 
+Route::get('/myfriend11', function () {
+    return'hello good morning 11';
+}); 
+Route::get('/myfriend12', function () {
+    return'hello good morning 12';
+}); 
